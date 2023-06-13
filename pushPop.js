05087d2096656a041f569a86ebf3c51ee0a9fd6c
fazlarabbyWD <fazla.rabbywd@gmail.com>
2023-06-13 21:58:45 +0600

    push-pop

---
+++ pushPop.js
@@ -0,0 +1,16 @@
+
+let numbers = [12, 54, 243, 86, 33];
+// numbers.push(76);
+
+console.log(numbers);
+
+let friends = ['Shisir', 'Rakib', 'Showrab'];
+// friends.push('Nayem');
+console.log(friends);
+
+numbers.pop();
+console.log(numbers);
+friends.pop();
+console.log(friends);
+
+
