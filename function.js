71e109b239f8e545e02324209acff0265d45c747
fazlarabbyWD <fazla.rabbywd@gmail.com>
2023-06-15 07:15:41 +0600

    function

---
+++ function.js
@@ -0,0 +1,7 @@
+
+// function dhori() {
+//     console.log('my name is Fazla Rabby');
+//     console.log('i am 25 years old');
+// }
+
+// dhori();
---
+++ pera.js
@@ -0,0 +1,15 @@
+// function give(tk) {
+//     console.log(tk)
+//     console.log('singara daw');
+// }
+// give(20);
+
+
+function calculation(a, b, c, d) {
+    let sum = a + b + c + d;
+    console.log(a, b, c, d);
+
+    console.log(sum);
+}
+
+calculation(12, 8, 4, 1);
